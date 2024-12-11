f0b88c8d8bfa9918983d3b21700acd0734245bb3
DPDmancul <davide.peressoni@tuta.io>
2024-12-11 23:16:04 +0100

    small fix

--- 2024/08-2.hs
+++ 2024/08-2.hs
@@ -47,28 +47,28 @@ import Data.List
 type Pos = (Int, Int)
 data Antenna = Antenna Char Pos
 
-antinodes :: Int -> Int ->[Antenna] -> [Pos]
+antinodes :: Int -> Int -> [Antenna] -> [Pos]
 antinodes rows cols = concatMap (concatMap pairAntinodes . pairs . map (\(Antenna _ pos) -> pos)) . groupBy eqFreq . sortBy ordFreq
-  where
-    ordFreq (Antenna f1 _) (Antenna f2 _) = compare f1 f2
-    eqFreq (Antenna f1 _) (Antenna f2 _) = f1 == f2
-    pairs [] = []
-    pairs (x : xs) = [(x, y) | y <- xs] ++ pairs xs
-    pairAntinodes ((r1, c1), a2@(r2, c2)) =
-        concat [[(r1 + i * stepRow, c1 + i * stepCol), (r1 - i * stepRow, c1 - i * stepCol)] | i <- [0 .. max rows cols]]
-      where
-        dr = r1 - r2
-        dc = c1 - c2
-        unit = gcd dr dc
-        stepRow = dr `div` unit
-        stepCol = dc `div` unit
+ where
+  ordFreq (Antenna f1 _) (Antenna f2 _) = compare f1 f2
+  eqFreq (Antenna f1 _) (Antenna f2 _) = f1 == f2
+  pairs [] = []
+  pairs (x : xs) = [(x, y) | y <- xs] ++ pairs xs
+  pairAntinodes ((r1, c1), (r2, c2)) =
+    concat [[(r1 + i * stepRow, c1 + i * stepCol), (r1 - i * stepRow, c1 - i * stepCol)] | i <- [0 .. max rows cols]]
+   where
+    dr = r1 - r2
+    dc = c1 - c2
+    unit = gcd dr dc
+    stepRow = dr `div` unit
+    stepCol = dc `div` unit
 
 main = interact $ show . length . nub . helper
-  where
-    helper file = filter inCity $ antinodes rows cols antennas
-      where
-        city = [Antenna f (r, c) | (r, line) <- zip [1 ..] (lines file), (c, f) <- zip [1 ..] line]
-        (rows, cols, antennas) = foldr visit (0, 0, []) city
-        inCity (r, c) = 0 < r && r <= rows && 0 < c && c <= cols
-        visit x@(Antenna f (r, c)) (rows, cols, antennas) =
-            (max rows r, max cols c, if f == '.' then antennas else x : antennas)
+ where
+  helper file = filter inCity $ antinodes rows cols antennas
+   where
+    city = [Antenna f (r, c) | (r, line) <- zip [1 ..] (lines file), (c, f) <- zip [1 ..] line]
+    (rows, cols, antennas) = foldr visit (0, 0, []) city
+    inCity (r, c) = 0 < r && r <= rows && 0 < c && c <= cols
+    visit x@(Antenna f (r, c)) (rows, cols, antennas) =
+      (max rows r, max cols c, if f == '.' then antennas else x : antennas)
